61314926c69bfbed7dd1b9f056671b331e8d618b
Sruthi <Sruthi@LAPTOP-2KKCO9Q0>
2022-12-05 15:09:36 +0530

    first commit

---
+++ myjavaprogram/src/sample/Hello.java
@@ -0,0 +1,14 @@
+package sample;
+
+public class Hello {
+	public void display()
+	{
+		System.out.println("hello world");
+	}
+
+	public static void main(String[] args) {
+		Hello obj=new Hello();
+		obj.display();
+	}
+
+}
